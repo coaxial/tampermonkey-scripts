427de751e87f50d120258277017868ab82b75ced
coaxial <c@64b.it>
2023-02-19 11:26:40 +0100

    Wait for reviews to load

--- ricardoch/adlinks.js
+++ ricardoch/adlinks.js
@@ -10,22 +10,55 @@
 // @homepage     https://github.com/coaxial/tampermonkey-scripts
 // ==/UserScript==
 
-(function () {
-  "use strict";
+"use strict";
 
-  [...document.querySelectorAll("p")]
-    .filter((e) =>
-      new RegExp("(N° d'article|Art.-Nr.|No.-art.) [0-9]+", "g").test(
-        e.innerText
-      )
-    )
-    .map((e) => {
-      const lang = location.toString().match(/(fr|de|it|en)/)[0];
-      const artNum = e.innerText.match(/[0-9]+/g);
-      const anchor = document.createElement("a");
-      anchor.href = `/${lang}/a/${artNum}/`;
-      anchor.innerText = artNum;
-      e.innerText = e.innerText.replace(artNum, "");
-      e.insertAdjacentElement("beforeEnd", anchor);
-    });
-})();
+// Some content is loaded later, this checks if reviews have been loaded yet
+// before running transformations on them.
+// Gives up after a minute.
+// Inspired from
+// https://github.com/Tampermonkey/tampermonkey/issues/1279#issuecomment-875386821
+function runWhenReady(elementFinder, callback) {
+  const numAttempts = 0;
+  const tryNow = function () {
+    const reviews = elementFinder();
+    const reviewsFound = reviews.length > 0;
+
+    if (reviewsFound) {
+      callback(reviews);
+    } else {
+      numAttempts++;
+      if (numAttempts >= 34) {
+        console.warn(
+          "Giving up after 34 attempts. Could not find: " + readySelector
+        );
+      } else {
+        setTimeout(tryNow, 250 * Math.pow(1.1, numAttempts));
+      }
+    }
+  };
+
+  tryNow();
+}
+
+function findReviews() {
+  // reviews are within `p` elements, but not all `p` are reviews.
+  [...document.querySelectorAll("p")].filter((e) =>
+    new RegExp("(N° d'article|Art.-Nr.|No.-art.) [0-9]+", "g").test(e.innerText)
+  );
+}
+
+function addLinks(reviews) {
+  reviews.map((e) => {
+    // Match current page language to link to the item in the corresponding
+    // language.
+    const lang = location.toString().match(/(fr|de|it|en)/)[0];
+    const artNum = e.innerText.match(/[0-9]+/g);
+    const anchor = document.createElement("a");
+    anchor.href = `/${lang}/a/${artNum}/`;
+    anchor.innerText = artNum;
+    e.innerText = e.innerText.replace(artNum, "");
+    e.insertAdjacentElement("beforeEnd", anchor);
+  });
+}
+
+runWhenReady(findReviews, addLinks);
